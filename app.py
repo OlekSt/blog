411961040e06ba58e4de021202bd8211a9cd677a
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-05-31 11:06:30 +0000

    Updated CRUD function vs ids

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI, Path
 from pydantic import BaseModel
-from typing import Text
+from typing import Optional, Text
 from datetime import datetime
 
 app = FastAPI()
 
-postdb = []
-
 
 # post model
-class Post(BaseModel):
-    id: int
+class Blog(BaseModel):
     title: str
     author: str
     content: Text
@@ -18,35 +15,55 @@ class Post(BaseModel):
     published_at: datetime
 
 
+class updateBlog(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    content: Optional[Text] = None
+    updated_at: datetime = datetime.now()
+    published_at: datetime
+
+
+blogdb = {}
+
+
 @app.get("/")
-async def read_root():
+async def home():
     return {"home": "Home page"}
 
 
 @app.get("/blog")
-async def get_posts():
-    return postdb
+async def get_blogs():
+    return blogdb
 
 
-@app.post("/blog")
-async def add_post(post: Post):
-    postdb.append(post.dict())
-    return postdb[-1]
+@app.post("/create-blog/{blog_id}")
+async def create_blog(blog_id: int, blog: Blog):
+    blogdb[blog_id] = blog
+    return blogdb[blog_id]
 
 
-@app.get("/blog/{post_id}")
-async def get_post(post_id: int = Path(..., gt=0)):
-    post = post_id - 1
-    return postdb[post]
+@app.get("/get-blog/{blog_id}")
+async def get_blog(blog_id: int = Path(None,
+                                       description="ID of your blog entry",
+                                       gt=0)):
+    return blogdb[blog_id]
 
 
-@app.put("/blog/{post_id}")
-def update_post(post_id: int, post: Post):
-    postdb[post_id - 1] = post
-    return {"message": "Post has been updated succesfully!"}
+@app.put("/update-blog/{post_id}")
+def update_blog(blog_id: int, blog: updateBlog):
+    if blog_id not in blogdb:
+        return {"Error": "Blog does not exist"}
+
+    if blog.title is not None:
+        blogdb[blog_id].title = blog.title
+    if blog.author is not None:
+        blogdb[blog_id].author = blog.author
+    if blog.content is not None:
+        blogdb[blog_id].content = blog.content
+    return {"message": "Blog has been updated succesfully!"}
 
 
-@app.delete("/blog/{post_id}")
-def delete_post(post_id: int):
-    postdb.pop(post_id-1)
-    return {"message": "Post has been deleted succesfully"}
+@app.delete("/delete-blog/{blog_id}")
+def delete_blog(blog_id: int):
+    blogdb.pop(blog_id)
+    return {"message": "Blog has been deleted succesfully"}
